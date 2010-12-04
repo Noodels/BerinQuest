5ceb54ba54879493f0c8a3bee44d73e7de238609
noodels <noodels@barjoi.home>
2010-12-04 16:18:08 +0000

    Added missing Room and Puppets, as well as prototype functions. Added a test folder for future test programs.

--- game/object.py
+++ game/object.py
@@ -5,3 +5,31 @@ class Object:
         self.loc = loc
         self.attributes = attribs
         self.contents = [ ]
+
+    def display(self, text):
+        pass
+
+    def moveTo(self, newLoc):
+
+    def pushItem(self, newItem):
+
+class Room(Object):
+    def __init__(self, world, loc, **attribs):
+        self.exits = { }
+        Object.__init__(self, world, loc, **attribs)
+
+    def renderExits(self):
+        return ", ".join(self.exits.keys())
+
+class Puppet(Object):
+    def __init__(self, world, loc, **attribs):
+        self.client = None
+        Object.__init__(self, world, loc, **attribs)
+
+    def display(self, text):
+        if self.client != None:
+            self.client.sendLine(text)
+
+    def registerClient(self, newClient):
+
+    def deregisterClient(self):
