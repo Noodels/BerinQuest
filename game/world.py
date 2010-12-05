4840ec54b8b5a8417fd8734b7a0ac92f059fe8f3
Berin Smaldon <bms508@york.ac.uk>
2010-12-05 11:36:09 +0000

    Added function names for world class as API begins to form Added project description to README

---
+++ game/world.py
@@ -0,0 +1,21 @@
+# World class, defines each world
+class World:
+    def __init__(self):
+        self.objects = [ ]
+        self.rooms = [ ]
+        self.puppets = [ ]
+        self.latestID = 0
+
+        self.connections = [ ]
+    
+    def getByID(self, identity):
+
+    def register(self, obj):
+        objects.append(obj)
+        if type(obj) == # TODO: Complete me
+
+    def deregister(self, obj):
+
+    def registerConnection(self, conn):
+
+    def deregisterConnection(self, conn):
